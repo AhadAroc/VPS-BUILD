fe99e85369ca6117514fa87713caeff319f7cbf7
AhadAroc <brothinkshecarti@gmail.com>
2025-04-05 16:37:48 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,6 +7,7 @@ const { setupMiddlewares } = require('./middlewares');
 const { setupCommands } = require('./commands');
 const mongoose = require('mongoose');
 const BOT_TOKEN = process.env.BOT_TOKEN;
+const Clone = require('./models/Clone');
 
 
 // Use this function to get the bot's data and update statistics
