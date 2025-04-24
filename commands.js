835b811c7332fce20e9245ef756c70fe20f9529a
AhadAroc <brothinkshecarti@gmail.com>
2025-04-25 01:06:03 +0300

    Update commands.js

--- commands.js
+++ commands.js
@@ -453,6 +453,7 @@ async function checkUserRank(ctx) {
 
 function setupCommands(bot) {
     const { setupActions, activeQuizzes, endQuiz,configureQuiz,startAddingCustomQuestions,chatStates, } = require('./actions'); // these were up there
+    const {isSubscribed } = require('./middlewares');
     bot.command('start', async (ctx) => {
         if (ctx.chat.type === 'private') {
             try {
@@ -2154,23 +2155,6 @@ bot.start(async (ctx) => {
 
 }
 
-async function isSubscribed(ctx, userId) {
-    try {
-        const channelUsername = 'ctrlsrc'; // Replace with your channel username
-        const member = await ctx.telegram.getChatMember(`@${channelUsername}`, userId);
-        const wasSubscribed = ctx.session.isSubscribed || false;
-        const isNowSubscribed = ['member', 'administrator', 'creator'].includes(member.status);
-        
-        ctx.session.isSubscribed = isNowSubscribed;
-        
-        return {
-            isSubscribed: isNowSubscribed,
-            statusChanged: wasSubscribed !== isNowSubscribed
-        };
-    } catch (error) {
-        console.error('Error checking subscription:', error);
-        return { isSubscribed: false, statusChanged: false };
-    }
-}
+
 module.exports = { setupCommands, isAdminOrOwner,showMainMenu,showQuizMenu,getLeaderboard,getDifficultyLevels, getQuestionsForDifficulty,isSecondaryDeveloper,isVIP,isSubscribed,chatBroadcastStates,awaitingBroadcastPhoto,updateActiveGroups };
 
