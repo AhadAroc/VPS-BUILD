365c8f8a24501220f0ae45d7ed27dd405af0b1d4
AhadAroc <brothinkshecarti@gmail.com>
2025-06-14 23:22:20 +0300

    Delete ecosystem.config.js

--- ecosystem.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  apps: [{
-    name: 'clone-bot',
-    script: 'clone_bt.js',
-    env: {
-      MONGO_URI: 'mongodb+srv://Amr:NidisuSI@cluster0.ay6fa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-    }
-  }]
-};
