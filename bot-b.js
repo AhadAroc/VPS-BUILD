bf1b07a29158e2e56253135842f832af62271ad3
AhadAroc <brothinkshecarti@gmail.com>
2025-06-14 16:31:51 +0300

    Update bot-b.js

--- bot-b.js
+++ bot-b.js
@@ -34,6 +34,6 @@ app.get('/ping', (req, res) => {
 });
 
 const PORT = 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Bot B server running on port ${PORT}`);
 });
