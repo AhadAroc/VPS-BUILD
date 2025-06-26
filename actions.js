0fa56c28ce109d3feeaaad2639ea012e2b398ea9
AhadAroc <brothinkshecarti@gmail.com>
2025-06-26 22:15:39 +0300

    Update actions.js

--- actions.js
+++ actions.js
@@ -6967,4 +6967,4 @@ bot.action('check_subscription', forceCheckSubscription);
 }
 
 module.exports = { setupActions,
-    activeQuizzes,endQuiz , ensureDatabaseInitialized,configureQuiz,startAddingCustomQuestions,chatStates,forceCheckSubscription,confirmSubscription,};
+    activeQuizzes,endQuiz , ensureDatabaseInitialized,configureQuiz,startAddingCustomQuestions,chatStates,forceCheckSubscription,confirmSubscription,updateUserActivity};
